0e71afd76eeb417205d7e1d5d50e04f9edcb5bf8
NAJISH  AKHTAR <166438726+najishakhtar2378@users.noreply.github.com>
2026-01-16 18:46:47 +0530

    Update notification.js

--- routes/notification.js
+++ routes/notification.js
@@ -9,5 +9,9 @@ router.get("/notifications", isLoggedIn, async (req, res) => {
 
   res.render("notifications/index", { notifications });
 });
+router.delete("/notifications/:id", async (req, res) => {
+  await Notification.findByIdAndDelete(req.params.id);
+  res.redirect("/notifications");
+});
 
 module.exports = router;
