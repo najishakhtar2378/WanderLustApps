5e223354d96adf18eb537200f59aa1b41b9b0ed6
NAJISH  AKHTAR <166438726+najishakhtar2378@users.noreply.github.com>
2026-01-25 07:27:37 +0530

    Delete utils/invoice.js

--- utils/invoice.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { downloadInvoice } = require("../controllers/invoiceController");
-
-router.get("/invoice/:id", downloadInvoice);
-
-module.exports = router;
